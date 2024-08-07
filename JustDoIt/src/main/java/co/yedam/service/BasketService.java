7a630fdbe3677b1458d2b6dfb092d5bdec627730
admin <admin@YD02-07>
2024-08-07 09:51:10 +0900

    1

--- JustDoIt/src/main/java/co/yedam/service/BasketService.java
+++ JustDoIt/src/main/java/co/yedam/service/BasketService.java
@@ -3,6 +3,5 @@ package co.yedam.service;
 import co.yedam.vo.BasketVO;
 
 public interface BasketService {
-	boolean addBasket(BasketVO basket);
 
 }
--- JustDoIt/src/main/java/co/yedam/service/BasketServiceImpl.java
+++ JustDoIt/src/main/java/co/yedam/service/BasketServiceImpl.java
@@ -6,14 +6,10 @@ import co.yedam.common.DataSource;
 import co.yedam.mapper.BasketMapper;
 import co.yedam.vo.BasketVO;
 
-public interface BasketServiceImpl implements BasketService {
+public class BasketServiceImpl implements BasketService {
 	SqlSession sqlSession = //
 			DataSource.getInstance().openSession(true); //자동으로 커밋하려고 true 넣음.
 	BasketMapper mapper = sqlSession.getMapper(BasketMapper.class);
 	
 	
-	@Override
-	public boolean addBasket(BasketVO basket) {
-		return mapper.insertBasket(basket) == 1;
-	}
 }
