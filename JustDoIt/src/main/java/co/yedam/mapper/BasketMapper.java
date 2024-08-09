7c395391d014fde644a2acdb74155166665d2b8d
admin <admin@YD02-06>
2024-08-09 09:33:24 +0900

    0809 커밋

--- JustDoIt/src/main/java/co/yedam/mapper/BasketMapper.java
+++
@@ -1,9 +0,0 @@
-package co.yedam.mapper;
-
-import co.yedam.vo.BasketVO;
-
-public interface BasketMapper {
-
-	int insertBasket(BasketVO basket);
-
-}
--- JustDoIt/src/main/java/co/yedam/service/BasketService.java
+++
@@ -1,7 +0,0 @@
-package co.yedam.service;
-
-import co.yedam.vo.BasketVO;
-
-public interface BasketService {
-
-}
