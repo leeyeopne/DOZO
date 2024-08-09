731a3160a5c3d0b10013e5f3847edba00397df1d
leeju <leeju@LAPTOP-ND2M0EGQ>
2024-08-09 09:32:14 +0900

    0808 ㅋㅅㅋ

--- JustDoIt/src/main/java/co/yedam/control/WishlistControl.java
+++ JustDoIt/src/main/java/co/yedam/control/WishlistControl.java
@@ -1,30 +1,50 @@
 package co.yedam.control;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import co.yedam.common.Control;
+import co.yedam.service.WishlistService;
+import co.yedam.service.WishlistServiceImpl;
 import co.yedam.vo.WishlistVO;
 
 public class WishlistControl implements Control {
 
 	@Override
 	public void exec(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		String wishColor = req.getParameter("wishColor");
-		String wishSize = req.getParameter("wishSize");
-		String wishQuantity = req.getParameter("wishQuantity");
+		// 요청 파라미터 읽기
 		String prodNo = req.getParameter("prodNo");
-		
-		WishlistVO wvo = new WishlistVO();
-		wvo.setWishColor(wishColor);
-		wvo.setWishSize(wishSize);
-		wvo.setWishQuantity(Integer.parseInt(wishQuantity));
-		wvo.setProdNo(Integer.parseInt(prodNo));
-		
+
+		// 위시 서비스 호출
+		WishlistService wvc = new WishlistServiceImpl();
+
+		Map<String, Object> map = new HashMap<>(); // 배열만들기
+
+		try {
+			if (wvc.addWishlist(Integer.parseInt(prodNo))) {
+				map.put("result", "Success"); //{"retCode" : true, student_name : "홍길동"}
+			} else {
+				map.put("result", "Fail");
+			}
+		} catch (Exception e) {
+			// 오류발생할때
+			map.put("result", "Fail");
+		}
+
+		// 결과값을 요청한곳(js)으로 보낼때 json 방식으로 보낸다.
+		Gson gson = new GsonBuilder().create();
+		String json = gson.toJson(map);
+
+		resp.getWriter().print(json);
+
 	}
 
 }
--- JustDoIt/src/test/java/co/yedam/common/AppTest.java
+++ JustDoIt/src/test/java/co/yedam/common/AppTest.java
@@ -2,16 +2,31 @@ package co.yedam.common;
 
 import org.apache.ibatis.session.SqlSession;
 
-import co.yedam.mapper.ProductMapper;
+import co.yedam.mapper.WishlistMapper;
+import co.yedam.vo.WishlistVO;
 
 public class AppTest {
 
 	public static void main(String[] args) {
-		SqlSession sqlSession = DataSource.getInstance().openSession(true); // 자동 커밋.
-		ProductMapper mapper = sqlSession.getMapper(ProductMapper.class);
-		mapper.selectList().forEach(product -> {
-			System.out.println(product);
-		});
+//		SqlSession sqlSession = DataSource.getInstance().openSession(true); // 자동 커밋.
+//		ProductMapper mapper = sqlSession.getMapper(ProductMapper.class);
+//		mapper.selectList().forEach(product -> {
+//			System.out.println(product);
+//		});
+//		
+//
+//		SqlSession sqlSession = //
+//				DataSource.getInstance().openSession(true); // 자동으로 커밋하려고 true 넣음.
+//		WishlistMapper mapper = sqlSession.getMapper(WishlistMapper.class);
+//
+//		WishlistVO brd = new WishlistVO();
+//		brd.setWishQuantity(1);
+//		brd.setWishColor("조건이");
+//		brd.setWishSize("2");
+//
+//		if (mapper.addWishlist(brd) == 1) {
+//			System.out.println("OK");
+//		}
 	}
 
 }
